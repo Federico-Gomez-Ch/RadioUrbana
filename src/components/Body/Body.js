0e2c6712974c30529076318763d60cfbfeee5bd2
federicogomez016 <federicogomez016@outlook.com>
2022-09-05 16:31:23 -0300

    firs commit

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,7 +2,20 @@ import React from 'react'
 import './Body.css'
 
 const Body = () => {
- return
+    
+    return (
+        <div className="container-card divInline" >
+            <div className="card" >
+                <img src= '../images/Max.jpeg' alt=""/>
+                <div className="contenido-card">
+                    <h3>Maxi Caput0</h3>
+                    <h6 >15:00 a 17:00 </h6>    
+                </div>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default Body
